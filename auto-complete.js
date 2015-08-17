839fd1e5bbc5e723549bd9170b356a6912859fd9
SimonSteinberger <simon.steinberger@outlook.com>
2015-08-17 13:52:51 +0200

    Auto escape Regex characters.

--- auto-complete.js
+++ auto-complete.js
@@ -36,6 +36,8 @@ var autoComplete = (function(){
             cache: 1,
             menuClass: '',
             renderItem: function (item, search){
+                // escape special characters
+                search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
                 var re = new RegExp("(" + search.split(' ').join('|') + ")", "gi");
                 return '<div class="autocomplete-suggestion" data-val="' + item + '">' + item.replace(re, "<b>$1</b>") + '</div>';
             },
