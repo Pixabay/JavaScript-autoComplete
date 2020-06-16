4fb1c1b003a82684dde9377652791402601ca403
Agnaldo Brito Junior <abj@icomp.ufam.edu.br>
2020-06-16 14:22:17 -0400

    Update utils/cache.js
    Co-authored-by: Felipe Matos Santana <felipems@yahoo.com.br>

--- utils/cache.js
+++ utils/cache.js
@@ -1,11 +1,7 @@
 function remmoveSuggestionFromCache(cache, suggestion) {
     for (key in cache) {
         cache[key] = cache[key].filter(function (element) {
-            if (element.titulo.replace(/<b>|<\/b>/g, '') !== suggestion) {
-                return true;
-            } else {
-                return false;
-            }
+            return element.titulo.replace(/<b>|<\/b>/g, '') !== suggestion;
         })
     }
 }
