9f98190e85b7cc012f9aae421cbf73df5478636d
Felipe Matos Santana <felipems@yahoo.com.br>
2020-07-20 23:34:44 -0300

    Fix demo

--- auto-complete.js
+++ auto-complete.js
@@ -75,7 +75,7 @@
                 // escape special characters
                 search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
                 var re = new RegExp("(" + search.split(' ').join('|') + ")", "gi");
-                return '<div class="autocomplete-suggestion" data-val="' + item + '">' + item.replace(re, "<b>$1</b>") + '</div>';
+                return '<div class="autocomplete-suggestion" data-val="' + item + '" data-index="' + suggestionIndex + '">' + item.replace(re, "<b>$1</b>") + '</div>';
             },
             onSelect: function (e, term, item) { },
             queryHistoryStorageName: null
