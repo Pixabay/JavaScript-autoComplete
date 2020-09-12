a74724333b41e963386788a5c5653af226fec3b9
Paul <paul.n.house@gmail.com>
2020-09-11 17:11:16 -0700

    Allowing selection of items with quotes
    Allowing selection of items with quotes and apostrophes

--- auto-complete.js
+++ auto-complete.js
@@ -41,7 +41,7 @@ var autoComplete = (function(){
                 // escape special characters
                 search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
                 var re = new RegExp("(" + search.split(' ').join('|') + ")", "gi");
-                return '<div class="autocomplete-suggestion" data-val="' + item + '">' + item.replace(re, "<b>$1</b>") + '</div>';
+                return '<div class="autocomplete-suggestion" data-val="' + item.replace("\"", "&quot;").replace("'", "&apos;") + '">' + item.replace(re, "<b>$1</b>") + '</div>';
             },
             onSelect: function(e, term, item){}
         };
